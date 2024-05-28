c2c9f29f88a7ef4ed65703e9ecfb2a2417409e16
nayeem <nayeemuddin.bd100@gmail.com>
2024-05-28 19:51:41 +0600

    feat: Home main section complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+import Container from "./components/ui/Container";
+import GradientButton from "./components/ui/GradientButton";
+import Heading from "./components/ui/Heading";
+
 export default function Home() {
+  //  h-[calc(100vh-90px)]
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className="text-3xl font-bold ">Hello world</h2>
-    </main>
+    <div className="w-full h-full bg-[url('/images/home-bg.jpg')] bg-cover bg-center bg-no-repeat mt-[90px] ">
+      <Container>
+        <div className="flex w-[80%] py-24 md:py-32 lg:py-32 text-white flex-col justify-center items-start">
+          <Heading label="Go for help" />
+          <h2 className="text-3xl md:text-7xl lg:text-[100px] font-semibold capitalize leading-none mt-3">
+            They are wait for some food.
+          </h2>
+          <p className="text-xl md:text-4xl mt-8 font-inter">
+            In a world of plenty, millions of poor people struggle daily with
+            hunger. They dream of simple meals, their hopes tethered to the
+            promise of food security.
+          </p>
+
+          <GradientButton label="Donate Now" path="/donate" />
+        </div>
+      </Container>
+    </div>
   );
 }
