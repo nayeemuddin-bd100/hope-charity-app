b18f6eda8df02a5f147f5b37b9de9c304f17396b
nayeem <nayeemuddin.bd100@gmail.com>
2024-06-06 23:14:22 +0600

    feat: add reusable gradient button

--- app/components/ui/GradientButton.tsx
+++ app/components/ui/GradientButton.tsx
@@ -15,16 +15,15 @@ const GradientButton = ({ label, path }: GradientButtonProps) => {
       className="text-lg bg-btn-gradient px-5 py-2 rounded-xl 
       mt-5 font-medium  transition-all      
       hover:before:bg-btn-gradient-hover
-   relative  
-   overflow-hidden  
-   shadow-2xl  before:absolute 
-   before:bottom-0 before:left-0 before:top-0
-   before:z-0 before:h-full before:w-0 
-   before:bg-btn-gradient before:transition-all 
-   before:duration-500 hover:text-white hover:before:left-0 
-   hover:before:w-full
-          
-          "
+      relative  
+      overflow-hidden  
+      shadow-2xl  before:absolute 
+      before:bottom-0 before:left-0 before:top-0
+      before:z-0 before:h-full before:w-0 
+      before:bg-btn-gradient before:transition-all 
+      before:duration-500 hover:text-white hover:before:left-0 
+      hover:before:w-full
+      "
     >
       <span className="relative z-10">
         {label} <GrFormNextLink className="inline w-3 h-3" />
