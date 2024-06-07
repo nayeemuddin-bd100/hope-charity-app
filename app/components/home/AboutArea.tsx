f6aba2d4e2958195dcf5f21cededaacb35c9a5a5
nayeem <nayeemuddin.bd100@gmail.com>
2024-06-07 16:50:18 +0600

    feat: complete about section

--- app/components/home/AboutArea.tsx
+++ app/components/home/AboutArea.tsx
@@ -7,19 +7,22 @@ const AboutArea = () => {
     <div className="relative">
       <Container>
         <div className="relative flex flex-col lg:flex-row  z-20 py-20 lg:py-0">
+          {/* Left Side Image */}
           <div className="w-full lg:w-5/12 flex justify-center items-center flex-col md:flex-col mx-auto relative  ">
             <div className=" hidden lg:block w-[300px]  h-full bg-btn-gradient absolute top-0"></div>
-            <div className="relative w-full  mx-auto   rounded-full overflow-hidden border-4 border-gray-50 shadow-2xl">
+            <div className="relative w-full  mx-auto rounded-full overflow-hidden ">
               <Image
                 alt="About Us Image"
                 src={"/images/about-us.jpg"}
-                className=" w-[80%] lg:w-full h-full object-cover rounded-full mx-auto"
+                className=" w-[80%] lg:w-full h-full object-cover rounded-full mx-auto border-8 shadow-none border-stone-300"
                 width="470"
                 height="470"
               />
             </div>
           </div>
-          <div className="w-full lg:w-6/12 flex flex-col justify-center items-center py-32">
+
+          {/* Right side */}
+          <div className="w-full lg:w-7/12 lg:px-5 lg:ml-5 flex flex-col justify-center items-center py-24">
             <div className="mb-6">
               <div className="flex justify-center lg:justify-start mt-5 lg:mt-0">
                 <Heading label="About Us" gradient />
@@ -31,48 +34,47 @@ const AboutArea = () => {
               </h2>
             </div>
             <p className="text-gray-600 mb-8 text-center md:text-left text-lg font-inter px-10 lg:px-0">
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout point
-              using Lorem Ipsum is that it has.
+              Dedicated to empowering children, Hope is a nonprofit organization
+              providing education, healthcare, and emotional support. We create
+              nurturing environments where every child can thrive and reach
+              their full potential. Join us in making a lasting difference in
+              children&apos;s lives worldwide.
             </p>
-            <div className="space-y-6 flex flex-col md:flex-row justify-around w-full lg:pr-5 items-center gap-5">
-              <div className="flex items-center flex-col md:flex-row  gap-y-3">
-                <div className="bg-primary-100  rounded-full mr-4 p-8 bg-green-100">
+
+            <div className="flex flex-col gap-10 md:flex-row lg:gap-0 justify-around items-center">
+              <div className="flex flex-col lg:flex-row justify-center items-center gap-5 lg:mr-5">
+                <div className="w-[100px] h-[100px] overflow-hidden relative rounded-full ">
                   <Image
                     src={"/images/ab-icon-1.png"}
-                    alt="Save Children Icon"
-                    width="54"
-                    height="54"
-                    decoding="async"
-                    loading="lazy"
+                    alt="icon"
+                    width={100}
+                    height={100}
+                    className="bg-green-100 p-4 rounded-full object-cover w-[98px] h-[98px]"
                   />
                 </div>
-                <div>
-                  <h2 className=" font-semibold text-gray-800 text-4xl">
-                    Save Children.
-                  </h2>
-                </div>
+                <h2 className="text-4xl font-bold text-gray-800">
+                  Save Children
+                </h2>
               </div>
-              <div className="flex items-center gap-y-3 flex-col md:flex-row  ">
-                <div className="p-8 rounded-full mr-4 bg-yellow-100">
+              <div className="flex flex-col lg:flex-row justify-start items-center gap-5 lg:mr-5">
+                <div className="w-[100px] h-[100px] overflow-hidden rounded-full ">
                   <Image
                     src={"/images/ab-icon-2.png"}
-                    alt="Fresh And Clean Water Icon"
-                    className=""
-                    width="54"
-                    height="54"
+                    alt="icon"
+                    width={100}
+                    height={100}
+                    className="bg-yellow-100 p-4 rounded-full object-cover w-[98px] h-[98px]"
                   />
                 </div>
-                <div>
-                  <h2 className="text-4xl text-gray-800 font-semibold">
-                    Fresh And Clean Water.
-                  </h2>
-                </div>
+                <h2 className="text-4xl font-bold text-gray-800 capitalize">
+                  Fresh and clean water
+                </h2>
               </div>
             </div>
           </div>
         </div>
 
+        {/* Right shape */}
         <div className="">
           <div className="hidden lg:block absolute top-0 right-0  h-full z-0  ">
             <Image
