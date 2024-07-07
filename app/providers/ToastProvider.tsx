056ebd4eb7109f42d088d697d7ecdf4644090733
nayeem <nayeemuddin.bd100@gmail.com>
2024-07-07 17:27:40 +0600

    feat: add toaster

---
+++ app/providers/ToastProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Toaster } from "react-hot-toast";
+
+const ToastProvider = () => {
+  return (
+    <div>
+      <Toaster position="top-right" />
+    </div>
+  );
+};
+
+export default ToastProvider;
