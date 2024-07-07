4c942f4e64127b79983d732c439c690965811115
nayeem <nayeemuddin.bd100@gmail.com>
2024-07-07 17:28:07 +0600

    feat: add custom loading

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-indigo-500 to-purple-600 flex flex-col justify-center items-center">
+      <div className="relative">
+        <div className="w-24 h-24 border-4 border-white rounded-full animate-ping absolute opacity-25"></div>
+      </div>
+    </div>
+  );
+}
