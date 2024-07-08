ba2eee9235533d7b53c690f077725ab3f94d85b0
nayeem <nayeemuddin.bd100@gmail.com>
2024-07-08 17:56:07 +0600

    feat: add utils for sever action

--- app/services/auth.service.ts
+++ app/services/auth.service.ts
@@ -1,8 +1,30 @@
 import { authKey } from "@/constant/authKey";
-import { setToLocalStorage } from "../../lib/localStorage";
+import { decodedToken } from "../../lib/jwtToken";
+import {
+  getFromLocalStorage,
+  removeFromLocalStorage,
+  setToLocalStorage,
+} from "../../lib/localStorage";
 
 export const storeToken = (accessToken: string) => {
-  console.log("🚀 ~ storeToken ~ accessToken:", accessToken);
-
   setToLocalStorage(authKey, accessToken);
+};
+
+export const getUserInfoFromToken = () => {
+  const authToken = getFromLocalStorage(authKey);
+  if (authToken) {
+    const decodedData = decodedToken(authToken);
+
+    return decodedData;
+  }
+};
+
+export const isLoggedIn = () => {
+  const authToken = getFromLocalStorage(authKey);
+
+  return !!authToken;
+};
+
+export const removeUser = () => {
+  return removeFromLocalStorage(authKey);
 };
