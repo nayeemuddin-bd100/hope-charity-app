873e2d7a9084366b903631d7a0f3d574efb7b2a7
nayeem <nayeemuddin.bd100@gmail.com>
2024-07-08 17:56:47 +0600

    feat: add jwt token and localstorage utils

--- lib/localStorage.ts
+++ lib/localStorage.ts
@@ -3,3 +3,15 @@ export const setToLocalStorage = (key: string, value: string) => {
 
   return localStorage.setItem(key, value);
 };
+
+export const getFromLocalStorage = (key: string) => {
+  if (!key || typeof window === "undefined") return;
+
+  return localStorage.getItem(key);
+};
+
+export const removeFromLocalStorage = (key: string) => {
+  if (!key || typeof window === "undefined") return;
+
+  return localStorage.removeItem(key);
+};
