11179b3758f39825e5b09c82b5e81ce1be4d507e
nayeem <nayeemuddin.bd100@gmail.com>
2024-07-12 03:33:36 +0600

    feat: redirect to login user

--- app/(withDashboardLayout)/layout.tsx
+++ app/(withDashboardLayout)/layout.tsx
@@ -1,10 +1,20 @@
+"use client";
+import { useRouter } from "next/navigation";
 import Sidebar from "../components/dashboard/Sidebar";
+import { getUserInfoFromToken, isLoggedIn } from "../services/auth.service";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  const userLoggedIn = isLoggedIn();
+  const userInfo = getUserInfoFromToken();
+
+  if (!userLoggedIn && userInfo?.role !== "admin") {
+    return router.push("/login");
+  }
   return (
     <div className="flex flex-col md:flex-row h-screen bg-gray-100 w-full  m-0 p-0 overflow-x-hidden">
       <div>
