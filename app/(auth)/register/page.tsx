b41cbb478f38f830dea4635490119a4e96e1c0cc
nayeem <nayeemuddin.bd100@gmail.com>
2024-07-13 13:27:23 +0600

    feat: add validation schema

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -3,62 +3,14 @@ import FormInput from "@/app/components/Form/FormInput";
 import FormWrapper from "@/app/components/Form/FormWrapper";
 import Container from "@/app/components/shared/Container";
 import registerUser from "@/app/services/actions/registerUser";
+import { registerZodSchema } from "@/app/validationSchema/authZodSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import toast from "react-hot-toast";
-import { z } from "zod";
 
-const registerZodSchema = z
-  .object({
-    firstName: z
-      .string()
-      .min(1, "First name is required")
-      .min(2, "First name must be at least 2 characters")
-      .max(30, "First name must be at most 30 characters")
-      .regex(/^[A-Za-z]+$/, "First name can only contain letters"),
-    lastName: z
-      .string()
-      .min(1, "Last name is required")
-      .min(2, "Last name must be at least 2 characters")
-      .max(30, "Last name must be at most 30 characters")
-      .regex(/^[A-Za-z]+$/, "Last name can only contain letters"),
-    email: z
-      .string()
-      .min(1, "Email is required")
-      .email("Please enter a valid email"),
-    address: z
-      .string()
-      .min(1, "Address is required")
-      .min(5, "Address must be at least 5 characters")
-      .max(100, "Address must be at most 100 characters"),
-    contactNo: z
-      .string()
-      .min(1, "Contact number is required")
-      .max(20, "Please enter a valid contact number")
-      .regex(/^[0-9+()\s-]+$/, "Please enter a valid contact number"),
-
-    password: z.string().min(6, "Password must be at least 6 characters"),
-    confirmPassword: z.string().min(1, "Confirm password is required"),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords don't match",
-    path: ["confirmPassword"],
-  });
-
-// profileImage: z
-//   .string()
-//   .min(1, "Profile image is required")
-//   .url("Invalid image URL"),
-// profileImage: z
-//   .instanceof(File, { message: "Profile image is required" })
-//   .refine((file) => file.size <= 1000000, `Max file size is 1MB.`)
-//   .refine(
-//     (file) => ["image/jpeg", "image/png"].includes(file.type),
-//     "Only .jpg and .png formats are supported."
-//   ),
 const Register = () => {
   const router = useRouter();
   const [userType, setUserType] = useState<"donor" | "volunteer">("donor");
