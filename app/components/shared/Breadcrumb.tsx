02d2971a0370c2611500dc39dbe5a8486a7ad471
nayeem <nayeemuddin.bd100@gmail.com>
2024-08-24 11:34:56 +0600

    fix: bug fixed to formate segment

--- app/components/shared/Breadcrumb.tsx
+++ app/components/shared/Breadcrumb.tsx
@@ -8,6 +8,18 @@ const Breadcrumb = ({ label }: { label: string }) => {
   const pathname = usePathname();
   const pathSegments = pathname.split("/").filter((segment) => segment !== ""); // ['about']
 
+  const formatSegment = (segment: string, index: number) => {
+    // Check if it's a donate page (to remove donate id from breadcrumb)
+    if (pathSegments[0] === "donate") {
+      if (index === 0) {
+        return "Donate";
+      } else if (index === 1) {
+        return "";
+      }
+    }
+    return segment;
+  };
+
   return (
     <div className="relative w-full h-[280px] mt-[90px]">
       <div className="absolute inset-0 bg-[url('/images/page-title.jpg')] bg-cover bg-center bg-no-repeat"></div>
@@ -29,15 +41,19 @@ const Breadcrumb = ({ label }: { label: string }) => {
               </div>
 
               <div>
-                {pathSegments.map((segment, index) => (
-                  <Link
-                    key={index}
-                    href={`/${pathSegments.slice(0, index + 1).join("/")}`}
-                    className=" text-gray-200 hover:text-gray-500 text-xl capitalize"
-                  >
-                    {segment}
-                  </Link>
-                ))}
+                {pathSegments.map((segment, index) => {
+                  const formattedSegment = formatSegment(segment, index);
+                  if (formattedSegment === "") return null; // Skip empty segments
+                  return (
+                    <Link
+                      key={index}
+                      href={`/${pathSegments.slice(0, index + 1).join("/")}`}
+                      className="text-gray-200 hover:text-gray-500 text-xl capitalize"
+                    >
+                      {formattedSegment}
+                    </Link>
+                  );
+                })}
               </div>
             </div>
           </div>
